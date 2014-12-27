8647a2d0abddb066ccb4a6092ac65e9cf0e01222
Mikkel Hoegh <mikkel@hoegh.org>
2014-12-27 18:59:02 +0100

    Implemented basic SQL cache.
    To save file system reqeuests for each query.

--- dialects/postgresql.js
+++ dialects/postgresql.js
@@ -5,8 +5,17 @@ var glob = require('glob');
 var path = require('path');
 var util = require('util');
 
+var sqlCache = {};
+
 module.exports = function (options) {
   var self = this;
+
+  var config = options || {};
+
+  // SQL cache enabled by default.
+  if (typeof(config.cache_sql) === "undefined") {
+    config.cache_sql = true;
+  }
 
   // Find files like {name}.sql or {name}.pgsql.
   // See https://www.npmjs.com/package/glob for format.
@@ -24,17 +33,17 @@ module.exports = function (options) {
       if (err) { callback(err); }
 
       if (params) {
-        options.client.query({
+        config.client.query({
           text: sql,
           values: params
         }, callback);
       }
       else {
-        options.client.query(sql, callback);
+        config.client.query(sql, callback);
       }
     });
   };
-  
+
   /**
    * Load the SQL query from file.
    *
@@ -42,25 +51,36 @@ module.exports = function (options) {
    * @param callback Node.js-style callback (function(err, sql)).
    */
   self.loadSql = function (name, callback) {
+    var cacheKey = config.sql_folder + ':' + name;
     var filePattern = util.format(self.filePattern, name);
+
+    if (config.cache_sql && sqlCache[cacheKey]) {
+      return callback(null, sqlCache[cacheKey]);
+    }
 
     // Find a SQL file matching the given name.
     glob(filePattern, {
-      cwd: options.sql_folder,
-      root: options.sql_folder,
+      cwd: config.sql_folder,
+      root: config.sql_folder,
       strict: true,
       nodir: true
     }, function (err, matches) {
       if (err) { return callback(err); }
 
       if (matches.length < 1) {
-        callback('Querious: No file matching `' + filePattern + '` found in folder `' + options.sql_folder + '`.');
+        callback('Querious: No file matching `' + filePattern + '` found in folder `' + config.sql_folder + '`.');
       }
       else if (matches.length > 1) {
-        return callback('Querious: Ambigious, multiple files matching `' + filePattern + '` found in folder `' + options.sql_folder + '`: ' + matches.join(', '));
+        return callback('Querious: Ambigious, multiple files matching `' + filePattern + '` found in folder `' + config.sql_folder + '`: ' + matches.join(', '));
       }
       else {
-        fs.readFile(path.join(options.sql_folder, matches[0]), 'utf-8', callback);
+        fs.readFile(path.join(config.sql_folder, matches[0]), 'utf-8', function (err, sql) {
+          if (!err && config.cache_sql) {
+            sqlCache[cacheKey] = sql;
+          }
+
+          return callback(err, sql);
+        });
       }
     });
   };
